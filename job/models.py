666b3544f7d3b24c470bfab71d8e5d1082300312
unknown <hossamelashry86@gmail.com>
2023-07-20 14:16:59 +0200

    upload media in job

--- job/models.py
+++ job/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 # Create your models here.
+def image_upload(instance,filename):
+    imagename , extension = filename.split(".")
+    return "jobs/%s.%s"%(instance.id,extension)
+
+
+
 JOB_TYPE=(('Full time','Full time'),('Part Time','Part Time'))
 class Job(models.Model): #as a table
     title=models.CharField(max_length=100) #colomn
@@ -11,7 +17,8 @@ class Job(models.Model): #as a table
     salary=models.IntegerField(default=0)
     experiance=models.IntegerField(default=1)
     category=models.ForeignKey('Category',on_delete=models.CASCADE)
-
+    #image=models.ImageField(upload_to='jobs/')
+    image=models.ImageField(upload_to=image_upload)
 
     def __str__(self):
         return self.title
--- job/views.py
+++ job/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
+from .models import Job
 # Create your views here.
+
+#model querey set
+
 def job_list(request):
-    pass
+    job_list=Job.objects.all()
+    #print(job_list)
+    context={'jobs':job_list} #template name
+    return render(request,'job/job_list.html',context)
 
 def job_details(request,id):#id to filter and get the wanted job
-    pass
+    job_detail=Job.objects.get(id=id)
+    context={'job':job_detail}
+    return render(request,'job/job_details.html',context)
